640cc1fa8db84c856df8eaa605d7a136b4146ccf
Joel Byler <jbyler@covermymeds.com>
2017-11-17 08:27:30 -0500

    Working phx 1.3

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :pointing_party, PointingParty.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "pointingparty.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :logger, level: :info
