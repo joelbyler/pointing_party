b8f03faef98a4209a0f42a67d048c1f78d5b28ed
Joel Byler <jbyler@covermymeds.com>
2017-11-24 10:42:54 -0500

    add non https config

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,9 @@ use Mix.Config
 
 config :pointing_party, PointingParty.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.pointing-party.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "www.pointing-party.com", port: 80],
+  # url: [scheme: "https", host: "www.pointing-party.com", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
