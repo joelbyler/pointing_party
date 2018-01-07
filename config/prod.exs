9f5125b486e70140887dec82f00bc896726efaf2
Joel Byler <jbyler@covermymeds.com>
2018-01-07 15:09:12 -0500

    Fix repo config for prod after upgrade

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :pointing_party,
 
 config :logger, level: :info
 
-config :pointing_party, PointingPartyWeb.Repo,
+config :pointing_party, PointingParty.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
